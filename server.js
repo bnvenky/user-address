09417d88f70db4af28b469979e5f179869483c58
bnvenky <bnvenkatesheee@gmail.com>
2024-09-30 09:28:44 +0530

    initial commit

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ dotenv.config()
 
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Error connecting to MongoDB:', err.message));
 
